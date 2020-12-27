b82628f7233e40f2ec703fbd8e2373ad92bafdf0
kamoshibito510 <kamoshibito510@icloud.com>
2020-12-27 14:22:47 +0900

    meta fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,12 +20,12 @@ const IndexPage = ({
     <Layout>
       <Helmet>
         <title>{site.siteMetadata.title}</title>
-        {/* <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@Gon387" /> 
-        <meta property="twitter:image" content="static/assets/computer_programming_man.png" />
+        <meta property="twitter:image" content="https://ecstatic-easley-e04553.netlify.app/static/assets/computer_programming_man.png" />
         <meta property="og:url" content="https://ecstatic-easley-e04553.netlify.app/" />
         <meta property="og:title" content="30代後半からはじめるエンジニア生活" /> 
-        <meta property="og:description" content="38歳未経験から独学＋プログラミングスクールを経てSES企業に入社しエンジニアとしての生活をスタートさせた筆者が、業務や日々の学習から学んだあれこれをアウトプットして記憶に定着させようと目論む（自称）テック系ブログです。" />  */}
+        <meta property="og:description" content="38歳未経験から独学＋プログラミングスクールを経てSES企業に入社しエンジニアとしての生活をスタートさせた筆者が、業務や日々の学習から学んだあれこれをアウトプットして記憶に定着させようと目論む（自称）テック系ブログです。" /> 
         
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
