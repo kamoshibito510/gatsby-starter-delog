0412f4631e2c2b2a46858199aa9a465eb2241beb
kamoshibito510 <kamoshibito510@icloud.com>
2021-01-10 22:49:25 +0900

    リンクの位置をレイアウトへ移動

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,9 @@ import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Navigation from "../components/navigation"
 import 'prismjs/themes/prism-okaidia.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitterSquare} from '@fortawesome/free-brands-svg-icons';
+
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -25,6 +28,11 @@ export default ({ children }) => {
       </header>
       {children}
       <footer className="site-footer">
+        <a href="https://twitter.com/Gon387?s=09">
+          <FontAwesomeIcon
+            color="#3eaded"
+            icon={faTwitterSquare} />
+        </a>   
         <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
       </footer>
     </div>
