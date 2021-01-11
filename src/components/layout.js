f0cc315920cc208fc8dacba16f3022c4e2f996e7
kamoshibito510 <kamoshibito510@icloud.com>
2021-01-11 15:23:33 +0900

    resize icon

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,6 +31,7 @@ export default ({ children }) => {
         <a href="https://twitter.com/Gon387?s=09">
           <FontAwesomeIcon
             color="#3eaded"
+            size="lg"
             icon={faTwitterSquare} />
         </a>   
         <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
